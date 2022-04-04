c44d5eb0400688dbc305d7d6853b1af19a88f70a
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-04 17:50:59 +0200

    Create Book class and add Rental array to it and method call "add_rentals"

---
+++ book.rb
@@ -0,0 +1,15 @@
+# This book class shows a Book as being an object in the Library(it hasa  title & author attribue & a rentals Array)
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rentals(person, date)
+    Rental.new(date, person, self)
+  end
+end
