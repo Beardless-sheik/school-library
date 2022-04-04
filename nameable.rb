791a899e775a08d0af44df52b03c49c65c716ed6
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-04 15:31:24 +0200

    Add Base_Decorator Class that extends Nameable

--- nameable.rb
+++ nameable.rb
@@ -3,3 +3,14 @@ class Nameable
     raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
   end 
 end
+
+class Base_Decorator < Nameable
+    # @param [Component] component
+    def initialize(component)
+      @namable = component
+    end
+
+    def operation
+      @namable.correct_name
+    end
+end
