527aa321679eeb8d32a3ad91206799bb46a5d9ef
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-12 16:28:59 +0200

    Create preserve_user_data function in App class to use the initialised data_manager to save the user data

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,8 @@ require_relative './person'
 
 # This Teacher class can be initialised by passing specialisation, Age, Name & parent permission.
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, parent_permission, name)
     @specialization = specialization
