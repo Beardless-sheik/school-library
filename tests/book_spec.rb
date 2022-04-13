0b3cb32548c9609536eb52fadf6b5777bd81a2a3
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-04-13 14:38:45 +0200

    Add unit tests to Teacher class
    - Fixed spelling errors in unit tests
    - Fixed rubocop errors

--- tests/book_spec.rb
+++ tests/book_spec.rb
@@ -1,45 +1,44 @@
 require_relative '../book'
 
-describe "Book Class" do 
+describe 'Book Class' do
   before :each do
-    @book = Book.new("Title", "Author")
+    @book = Book.new('Title', 'Author')
   end
 
-   context "With valid input to #new" do 
-      it "instance created should be a book object that has a class of Books" do
-         instanse_class = @book.class 
+  context 'With valid input to #new' do
+    it 'instance created should be a book object that has a class of Books' do
+      instance_class = @book.class
 
-         expect(instanse_class).to be Book 
-      end 
-		
-      it "instance created should have a title isntance property as what is given" do 
-         instanse_property = @book.title
-
-         expect(instanse_property).to eq "Title"
-      end 
-      it "instance created should have an rentals instance property that is an empty array" do
-        instanse_property = @book.rentals
-
-        expect(instanse_property).to eq []
-      end
-   end 
-
-   context "with valid input #add_rentals" do
-     it "rentals instance property should have one element " do
-        alick = double('Person', age:20, parent_permission:true, name:"Alick", rentals:[])
-        @book.add_rentals(alick,"10 December")
-        instanse_property = @book.rentals
-
-        expect(instanse_property.length).to eq 1
-     end
-
-     it "the first element in the rentals instance property should have a class rental" do
-      alick = double('Person', age:20, parent_permission:true, name:"Alick", rentals:[])
-      @book.add_rentals(alick,"10 December")
-      instanse_property_class = @book.rentals[0].class
-      
-      expect(instanse_property_class).to be Rental
+      expect(instance_class).to be Book
     end
-   end
 
+    it 'instance created should have a title isntance property as what is given' do
+      instance_property = @book.title
+
+      expect(instance_property).to eq 'Title'
+    end
+    it 'instance created should have an rentals instance property that is an empty array' do
+      instance_property = @book.rentals
+
+      expect(instance_property).to eq []
+    end
+  end
+
+  context 'with valid input #add_rentals' do
+    it 'rentals instance property should have one element ' do
+      alick = double('Person', age: 20, parent_permission: true, name: 'Alick', rentals: [])
+      @book.add_rentals(alick, '10 December')
+      instance_property = @book.rentals
+
+      expect(instance_property.length).to eq 1
+    end
+
+    it 'the first element in the rentals instance property should have a class rental' do
+      alick = double('Person', age: 20, parent_permission: true, name: 'Alick', rentals: [])
+      @book.add_rentals(alick, '10 December')
+      instance_property_class = @book.rentals[0].class
+
+      expect(instance_property_class).to be Rental
+    end
+  end
 end
--- tests/student_spec.rb
+++ tests/student_spec.rb
@@ -1,29 +1,28 @@
 require_relative '../student'
 
-describe "Student Class" do 
+describe 'Student Class' do
   before :each do
-    @student = Student.new("English", 20,true,"Alick")
+    @student = Student.new('English', 20, true, 'Alick')
   end
 
-   context "With valid input to #new" do 
-      it "instance created should be a Student object that has a class of Student" do
-         instanse_class = @student.class 
+  context 'With valid input to #new' do
+    it 'instance created should be a Student object that has a class of Student' do
+      instance_class = @student.class
 
-         expect(instanse_class).to be Student 
-      end 
-		
-      it "instance created should have a classroom isntance property as what is given" do 
-         instanse_property = @student.classroom
+      expect(instance_class).to be Student
+    end
 
-         expect(instanse_property).to eq "English"
-      end 
-   end 
+    it 'instance created should have a classroom isntance property as what is given' do
+      instance_property = @student.classroom
 
-   context "with valid input #classrom" do
-     it "it should return instance property classroom with correct value given when initialising " do
-        instanse_property_method_return = @student.classroom
-        expect(instanse_property_method_return).to eq "English"
-     end
-   end
+      expect(instance_property).to eq 'English'
+    end
+  end
 
+  context 'with valid input #classrom' do
+    it 'it should return instance property classroom with correct value given when initialising ' do
+      instance_property_method_return = @student.classroom
+      expect(instance_property_method_return).to eq 'English'
+    end
+  end
 end
